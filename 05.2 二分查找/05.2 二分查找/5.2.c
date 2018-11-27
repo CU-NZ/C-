d026bfee84e60eb89e90fdef43c1023c7918c05e
cunz <645766176@qq.com>
2018-11-27 21:58:31 +0800

    二分查找c语言实现

---
+++ 05.2 二分查找/05.2 二分查找/5.2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int binary_search(int arr[], int key, int left, int right)
+{
+
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			return mid;
+		}
+
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int k = 7;
+	int ret = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	ret = binary_search(arr, k, left, right);
+	if (-1 == ret)
+	{
+		printf("找不到\n");
+	}
+	else
+	{
+		printf("找到了:%d\n", ret);
+	}
+	system("pause");
+	return 0;
+}
